43d28e615bc16cc52f930049e5109b2acdf08198
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2023-03-20 08:57:45 +0000

    [Making]ユーザー登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  get 'home/about' => "homes#about"
   devise_for :users
   resources :users, only: [:show, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :destroy]
