e580c5155f11544bd27766c6293ef294a4b5f970
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2023-03-21 06:18:05 +0000

    [Fin]/books/:id

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,11 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     book.user_id = current_user.id
     book.save
-    redirect_to books_path
+    redirect_to book_path(book.id)
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   private
