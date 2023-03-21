0d9b2420ad70a3cb01f58c0a07061fd4283bbf23
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2023-03-21 12:15:29 +0000

    [Fin]books編集機能、update機能、削除機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,6 +25,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
