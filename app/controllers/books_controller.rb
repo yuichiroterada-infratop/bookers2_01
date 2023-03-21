a4d0942ac80beaba85754621524ff13041e7ca4e
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2023-03-21 05:12:36 +0000

    [Fin]users/showのviewページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,17 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- db/migrate/20230320050359_create_books.rb
+++ db/migrate/20230320050359_create_books.rb
@@ -1,8 +1,6 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-
-      t.integer :book_id
       t.string :title
       t.text :body
       t.integer :user_id
