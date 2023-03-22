27162fbea9e4dda0962df6e0d3f6f71e4d87b2f8
EC2 Default User <ec2-user@ip-172-31-13-223.ap-northeast-1.compute.internal>
2023-03-22 14:38:25 +0000

    [Start]RSpec test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to books_path
+    end
   end
 
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :name, presence: true
+  validates :name, length: {minimum: 2, maximum: 20}, uniqueness: true
+  validates :introduction, length: {maximum: 50}
+  
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
   def get_profile_image(size)
