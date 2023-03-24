86f3d53cac5c45228a950c6eb7a914ef34cc8fc2
EC2 Default User <ec2-user@ip-172-31-13-223.ap-northeast-1.compute.internal>
2023-03-24 04:47:01 +0000

    [Fin]これ以上進められない/後は質問対応してもらう

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,13 @@ class User < ApplicationRecord
   
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
+  
   def get_profile_image(size)
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      file_path = Rails.root.join('app/assets/images/default-image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/png')
     end
-    profile_image.variant(resize_to_limit: [100, 100]).processed
+    profile_image.variant(resize: size).processed
   end
 
 end
